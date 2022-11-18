d0eced2ce4d9497984df041589972e22300a393f
ayazi01 <marwanewahbi@gmail.com>
2022-11-18 16:29:05 -0500

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/'
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2d7e7959-5121-4502-ba9b-cb3bcba08888';
+export const MODAL_CLOSE_SEC = 2.5;
